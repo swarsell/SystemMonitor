3dfc1be39b1f12e46e2189fe3447669cb24c6cad
swarsell <swarsellsocial@gmail.com>
2026-04-24 14:06:57 +0300

    Add files via upload

---
+++ SystemMonitor/SystemMonitor/SystemMonitor.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <filesystem>
+#include <thread>
+#include <chrono>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+void printMemoryStats() {
+	MEMORYSTATUSEX memInfo;
+	memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+	GlobalMemoryStatusEx(&memInfo);
+
+	long long totalPhysMem = memInfo.ullTotalPhys / 1024 / 1024 / 1024;
+	long long physMemUsed = (memInfo.ullTotalPhys - memInfo.ullAvailPhys) / 1024 / 1024 / 1024;
+	int memPercent = memInfo.dwMemoryLoad;
+
+	fs::space_info cd = fs::space("C:\\");
+	long long totalDisk = cd.capacity / 1024 / 1024 / 1024;
+	long long freeDisk = cd.available / 1024 / 1024 / 1024;
+
+	cout << "\033[H";
+	cout << "loading memory: " << memPercent << "%\n";
+	cout << "used: " << physMemUsed << "GB from " << totalPhysMem << "GB\n";
+	cout << "disk " << totalDisk - freeDisk << "GB from " << totalDisk << "GB\n";
+}
+
+int main() {
+	while (true) {
+		printMemoryStats();
+		this_thread::sleep_for(chrono::milliseconds(1000));
+	}
+}
